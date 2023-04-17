7414e7f150db9f1af50acbd4e2564fe9728e4a61
dmcomm <71192617+dmcomm@users.noreply.github.com>
2023-04-17 03:09:50 +0100

    Add basic DMC support (#29)

--- lib/dmcomm/hardware/prongs.py
+++ lib/dmcomm/hardware/prongs.py
@@ -38,7 +38,7 @@ class ProngCommunicator:
 				init=pio_programs.do_nothing,  # workaround for CircuitPython bug
 			)
 			self._output_weak_pull.value = self._params.idle_state
-			self._input_pulses = pulseio.PulseIn(self._pin_input, maxlen=40, idle_state=self._params.idle_state)
+			self._input_pulses = pulseio.PulseIn(self._pin_input, maxlen=260, idle_state=self._params.idle_state)
 			self._input_pulses.pause()
 		except:
 			self.disable()
@@ -53,9 +53,13 @@ class ProngCommunicator:
 		#self._output_weak_pull = None
 		self._input_pulses = None
 		self._enabled = False
-	def send(self, bits):
+	def send(self, data):
 		if not self._enabled:
 			raise RuntimeError("not enabled")
+		try:
+			iter(data)
+		except TypeError:
+			data = [data]
 		if self._params.idle_state == True:
 			DRIVE_ACTIVE = 0
 			DRIVE_INACTIVE = 1
@@ -66,20 +70,24 @@ class ProngCommunicator:
 		array_to_send = array.array("L", [
 			DRIVE_INACTIVE, self._params.pre_high_send,
 			DRIVE_ACTIVE, self._params.pre_low_send,
-			DRIVE_INACTIVE, self._params.start_high_send,
-			DRIVE_ACTIVE, self._params.start_low_send,
 		])
-		for i in range(16):
+		if self._params.start_high_send is not None:
 			array_to_send.append(DRIVE_INACTIVE)
-			if bits & 1:
-				array_to_send.append(self._params.bit1_high_send)
-				array_to_send.append(DRIVE_ACTIVE)
-				array_to_send.append(self._params.bit1_low_send)
-			else:
-				array_to_send.append(self._params.bit0_high_send)
-				array_to_send.append(DRIVE_ACTIVE)
-				array_to_send.append(self._params.bit0_low_send)
-			bits >>= 1
+			array_to_send.append(self._params.start_high_send)
+			array_to_send.append(DRIVE_ACTIVE)
+			array_to_send.append(self._params.start_low_send)
+		for bits in data:
+			for i in range(16):
+				array_to_send.append(DRIVE_INACTIVE)
+				if bits & 1:
+					array_to_send.append(self._params.bit1_high_send)
+					array_to_send.append(DRIVE_ACTIVE)
+					array_to_send.append(self._params.bit1_low_send)
+				else:
+					array_to_send.append(self._params.bit0_high_send)
+					array_to_send.append(DRIVE_ACTIVE)
+					array_to_send.append(self._params.bit0_low_send)
+				bits >>= 1
 		array_to_send.append(DRIVE_INACTIVE)
 		array_to_send.append(self._params.cooldown_send)
 		array_to_send.append(RELEASE)
@@ -92,14 +100,14 @@ class ProngCommunicator:
 		pulses.resume()
 		if timeout_ms == WAIT_REPLY:
 			timeout_ms = self._params.reply_timeout_ms
-		misc.wait_for_length_2(pulses, 35, timeout_ms, self._params.packet_length_timeout_ms)
+		misc.wait_for_length_2(pulses, self._params.pulses_expected, timeout_ms, self._params.packet_length_timeout_ms)
 		pulses.pause()
 		if len(pulses) == pulses.maxlen:
 			raise ReceiveError("buffer full")
 		if len(pulses) == 0:
-			return None
+			return self._params.empty_value
 		ic_bug = False
-		if len(pulses) < 35:
+		if len(pulses) < self._params.pulses_expected:
 			if self._params.signal_type == "X" and len(pulses) == 34:
 				ic_bug = True
 			else:
@@ -107,6 +115,12 @@ class ProngCommunicator:
 		t = pulses.popleft()
 		if t < self._params.pre_low_min:
 			raise ReceiveError("pre_low = %d" % t)
+		if self._params.signal_type == "C":
+			return self._receive_color()
+		else:
+			return self._receive_classic(ic_bug)
+	def _receive_classic(self, ic_bug):
+		pulses = self._input_pulses
 		t = pulses.popleft()
 		if t < self._params.start_high_min or t > self._params.start_high_max:
 			raise ReceiveError("start_high = %d" % t)
@@ -129,6 +143,25 @@ class ProngCommunicator:
 		if self._params.invert_bit_read:
 			result ^= 0xFFFF
 		return result
+	def _receive_color(self):
+		pulses = self._input_pulses
+		results = []
+		bit_count = 0
+		while len(pulses) > 0:
+			result = 0
+			for i in range(16):
+				t = pulses.popleft()
+				if t < self._params.bit_high_min or t > self._params.bit_high_max:
+					raise ReceiveError("bit_high %d = %d" % (bit_count + 1, t))
+				t = pulses.popleft()
+				if t < self._params.bit_low_min or t > self._params.bit_low_max:
+					raise ReceiveError("bit_low %d = %d" % (bit_count + 1, t))
+				result >>= 1
+				if t > self._params.bit_low_threshold:
+					result |= 0x8000
+				bit_count += 1
+			results.append(result)
+		return results
 
 class ProngParams:
 	def __init__(self):
@@ -154,11 +187,14 @@ class ProngParams:
 			self.bit_high_max = 3400
 			self.bit_low_min = 1000
 			self.bit1_low_send = 1667
+			self.bit_low_threshold = None
 			self.bit0_low_send = 3167
 			self.bit_low_max = 4000
 			self.cooldown_send = 400
 			self.reply_timeout_ms = 100
 			self.packet_length_timeout_ms = 300
+			self.pulses_expected = 35
+			self.empty_value = None
 		elif signal_type == "X":
 			self.idle_state = True
 			self.invert_bit_read = False
@@ -179,11 +215,14 @@ class ProngParams:
 			self.bit_high_max = 5500
 			self.bit_low_min = 1200
 			self.bit1_low_send = 1600
+			self.bit_low_threshold = None
 			self.bit0_low_send = 4000
 			self.bit_low_max = 5500
 			self.cooldown_send = 400
 			self.reply_timeout_ms = 200  # but wait_for_length_2 failing at 40 when this was 100?
 			self.packet_length_timeout_ms = 300
+			self.pulses_expected = 35
+			self.empty_value = None
 		elif signal_type == "Y":
 			self.idle_state = False
 			self.invert_bit_read = True
@@ -204,11 +243,42 @@ class ProngParams:
 			self.bit_high_max = 4500
 			self.bit_low_min = 1200
 			self.bit1_low_send = 4400
+			self.bit_low_threshold = None
 			self.bit0_low_send = 1600
 			self.bit_low_max = 5000
 			self.cooldown_send = 200
 			self.reply_timeout_ms = 100
 			self.packet_length_timeout_ms = 300
+			self.pulses_expected = 35
+			self.empty_value = None
+		elif signal_type == "C":
+			self.idle_state = True
+			self.invert_bit_read = None
+			self.pre_high_send = 1000
+			self.pre_low_min = 65535
+			self.pre_low_send = 150000
+			#self.pre_low_max? PulseIn only goes up to 65535
+			self.start_high_min = None
+			self.start_high_send = None
+			self.start_high_max = None
+			self.start_low_min = None
+			self.start_low_send = None
+			self.start_low_max = None
+			self.bit_high_min = 200
+			self.bit0_high_send = 500
+			self.bit_high_threshold = None
+			self.bit1_high_send = 500
+			self.bit_high_max = 700
+			self.bit_low_min = 200
+			self.bit1_low_send = 1500
+			self.bit_low_threshold = 1000
+			self.bit0_low_send = 500
+			self.bit_low_max = 1700
+			self.cooldown_send = 500
+			self.reply_timeout_ms = 200  # why?
+			self.packet_length_timeout_ms = 400
+			self.pulses_expected = 257
+			self.empty_value = []
 		else:
 			raise ValueError("signal_type must be V/X/Y")
 		self.signal_type = signal_type
